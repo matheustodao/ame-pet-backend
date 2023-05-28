904b916acd1e91291f2f34b991d4588fd404066e
matheustodao <todaomatheus@hotmail.com>
2023-05-28 04:23:50 -0300

    feat: add get nearby veterinaries clinics

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,8 @@ import { createIncident } from '../useCases/Incident/create';
 import { getByIdIncident } from '../useCases/Incident/getById';
 import { changeStatusIncident } from '../useCases/Incident/changeStatus';
 import { AuthMiddleware } from '../middleware/authMiddleware';
+import axios from 'axios';
+import { getNearbyClinics } from '../useCases/maps/places/getNearbyClinics';
 
 const router = Router();
 
@@ -19,9 +21,12 @@ router.use('/incident', AuthMiddleware,
   router.patch('/:id', changeStatusIncident),
 )
 
+router.use('/maps', AuthMiddleware,
+  router.get('/nearby/clinics', getNearbyClinics)
+)
+
 router.post('/login', loginUser)
 
 router.post('/ong', createOng)
-
 
 export { router };
---
+++ src/useCases/maps/places/getNearbyClinics.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import { googleMapsApi } from '../../../configs/api/googleMaps';
+
+export async function getNearbyClinics(req: Request, res: Response) {
+  const { lat, lng } = req.query;
+
+  if (!lat || !lng) {
+    return res.status(400).json({ error: 'Missing query lat and lng' });
+  }
+
+  const clientLocation = `${lat}, ${lng}`
+
+  const { data: { results: rawPlaces } } = await googleMapsApi.get('/place/nearbysearch/json', {
+    params: {
+      location: clientLocation,
+      type: 'veterinary_care|pet_store',
+      radius: 5000,
+      opennow: '1'
+    }
+  });
+
+  async function distanceMatrix(destination: string, origin: string) {
+    const { data: distance } = await googleMapsApi.get('/distancematrix/json', {
+      params: {
+        origins: origin,
+        destinations: destination
+      }
+    })
+
+    return {
+      distanceLength: distance.rows[0].elements[0].distance.text
+    }
+  }
+
+  const places = await Promise.all(rawPlaces.map(async (place: any) => {
+    const placeLocation = `${place.geometry.location.lat}, ${place.geometry.location.lng}`;
+    const { distanceLength } = await distanceMatrix(placeLocation, clientLocation);
+
+    return {
+      id: place.id,
+      name: place.name,
+      images: place.photos,
+      rating: place.rating,
+      location: placeLocation,
+      distanceLength
+    }
+  }))
+
+  res.json(places)
+}
